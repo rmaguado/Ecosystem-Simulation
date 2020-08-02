3cd3a91d1dea8bef1611982976a24f7286509d09
Rubén Moreno aguado <68761857+rmaguado@users.noreply.github.com>
2020-08-02 18:33:28 +0200

    Update params.py

--- params.py
+++ params.py
@@ -12,6 +12,7 @@ class Params():
     Parameter Class
     """
     def __init__(self):
+
         self.grid_size = 20
         self.starting_creatures = 20
         self.simulate = False
@@ -19,7 +20,7 @@ class Params():
         self.window_show = True
 
         self.general_nn = True
-        self.inherit_nn = None
+        self.inherit_nn = None # "weights-2020.07.31-12.41.36.model"
         self.batch_size = 128
         self.exploration_rate = 0.01
         self.learning_rate = 0.05
