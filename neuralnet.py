eb14218008b74a90811907964cd1185604bc5f04
Victor Moreno <v.moreno@iconcologia.net>
2020-08-08 19:53:24 +0200

    Adam optimizer

--- neuralnet.py
+++ neuralnet.py
@@ -18,8 +18,11 @@ class DeepQNetwork(nn.Module):
         self.params = Params()
 
         self.device = T.device(self.params.cuda if T.cuda.is_available() else 'cpu')
-
+        
+        
+        # network model
         if self.params.convolutional:
+            
             self.conv1 = nn.Conv2d(in_channels=self.params.state_features, out_channels=16, padding=1, kernel_size=3, stride=1)
             self.conv2 = nn.Conv2d(in_channels=16, out_channels=4, padding=1, kernel_size=3, stride=1)
             self.conv3 = nn.Conv2d(in_channels=4, out_channels=1, padding=1, kernel_size=3, stride=1)
@@ -27,17 +30,42 @@ class DeepQNetwork(nn.Module):
             self.fc1 = nn.Linear(fc_input_dims, 512)
             self.fc2 = nn.Linear(512, self.params.action_size)
         else:
-            fc_input_dims = self.params.state_features * self.params.vision_grid * self.params.vision_grid
-            self.fc1 = nn.Linear(fc_input_dims, 512)
-            self.fc2 = nn.Linear(512, 256)
-            self.fc3 = nn.Linear(256, self.params.action_size)
+            
+            fc_input_dims = self.params.state_features * self.params.vision_grid * self.params.vision_grid # 100
+            self.fc1 = nn.Linear(fc_input_dims, 400)
+            self.fc2 = nn.Linear(400, 300)
+            self.fc3 = nn.Linear(300, self.params.action_size)
 
-        self.optimizer = optim.RMSprop(self.parameters(), lr=self.params.learning_rate)
+        self.optimizer = optim.Adam(self.parameters(), lr=self.params.learning_rate)
+        # RMSprop
 
         self.loss = nn.MSELoss()
         
         self.to(self.device)
 
+    def forward(self, state):
+        """
+        Forward propagation.
+        """
+        if self.params.convolutional:
+            
+            o = F.relu(self.conv1(state)) # conv1
+            o = F.relu(self.conv2(o))     # conv2
+            o = F.relu(self.conv3(o))     # conv3      shape is BS x n_filters x H x W
+            o = o.view(o.size()[0], -1)   # conv_flat  shape is BS x (n_filters * H * W)
+            o = F.relu(self.fc1(o))       # relu1      -> 512
+            actions = self.fc2(o)         # sum    512 -> 6
+            
+        else:
+            
+            state = T.flatten(state, 1)  # flatten starting 1: leaving the batch
+            
+            o = F.relu(self.fc1(state))  # relu1      -> 400
+            o = F.relu(self.fc2(o))      # relu2  400 -> 300
+            actions = self.fc3(o)        # sum    300 -> 6
+
+        return actions
+    
     def calculate_conv_output_dims(self, input_dims):
         """
         Returns dimensions of convolutional output.
@@ -48,22 +76,3 @@ class DeepQNetwork(nn.Module):
         dims = self.conv3(dims)
         return int(np.prod(dims.size()))
 
-    def forward(self, state):
-        """
-        Forward propagation.
-        """
-        if self.params.convolutional:
-            conv1 = F.relu(self.conv1(state))
-            conv2 = F.relu(self.conv2(conv1))
-            conv3 = F.relu(self.conv3(conv2))            # conv3      shape is BS x n_filters x H x W
-            conv_state = conv3.view(conv3.size()[0], -1) # conv_state shape is BS x (n_filters * H * W)
-            flat1 = F.relu(self.fc1(conv_state))
-            actions = self.fc2(flat1)
-        else:
-            state = T.flatten(state, 1)  # flatten starting 1: leaving the batch
-            flat1 = F.relu(self.fc1(state))
-            flat2 = F.relu(self.fc2(flat1))
-            actions = self.fc3(flat2)
-
-        return actions
-    
