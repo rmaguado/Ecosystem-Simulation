9e0c707f643a1add6a489af47c8a50f52c26058a
Victor Moreno <h501uvma@mmini-VM.local>
2020-08-16 16:37:47 +0200

    Revised logs

--- out/read.R
+++ out/read.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 
-l<-as.data.frame(data.table::fread(args[1], na.strings = ".", fill=T))
+l <- as.data.frame(data.table::fread(args[1], na.strings = ".", fill=T))
 
-options(width=150)
+options(width = 150)
 
 cat("\nAll\n\n")
 
@@ -15,7 +15,7 @@ Table(l$action, l$reward)
 
 
 cat("\nLast 25%\n\n")
-l<-l[ round(NROW(l)*.75):NROW(l),]
+l <- l[round(NROW(l)*.75):NROW(l), ]
 
 summary(l)
 Table(l$repro == 0)
@@ -24,10 +24,9 @@ Table(l$action)
 Table(l$action, l$reward)
 
 cat("\nLast 25% no random\n\n")
-l<-l[ l$repro!=0,]
+l <- l[l$repro != 0, ]
 
 summary(l)
 Table(l$creatures)
 Table(l$action)
 Table(l$action, l$reward)
-
